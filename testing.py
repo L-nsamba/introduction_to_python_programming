efcf8e3d82a1adc1a9a6f25bf8860ad27968b837
L-nsamba <l.nsamba@alustudent.com>
2025-06-18 11:14:34 +0200

    First Commit

---
+++ testing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+#if 5 > 4:
+    #print("Five is greater than four")
+
+"""This is a comment 
+written in more
+than just one line"""
+#print("Hello, World!")
+
+#name = "Gamma AI"
+#name = "Alpha AI"
+#print(name)
+
+"""an_int = 27
+a_float = 3.0
+quotient = an_int / a_float
+print(quotient == an_int / a_float) #True
+
+print(3**3 + 3 > 30) #False
+print(3**3 + 3 <= 30) #False
+print('hello' != 'Hello') #True"""
+
+
+the_answer = 7
+an_int = 7000
+a_float = 1000
+
+print(the_answer == (an_int/ a_float))
