9b50910af556131aaaf5e432da91fb90f1feed0a
L-nsamba <l.nsamba@alustudent.com>
2025-07-07 11:57:51 +0200

    Updates

---
+++ python_practice/multiplication.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+number = int(input("Enter your number of choice: "))
+
+for i in range(1, 13):
+    print(f"{number} * {i} = {number * i}")
---
+++ python_practice/tuple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+student_grades = (10, 9, 8, 9, 3, 6, 2)
+
+print(student_grades[2])
